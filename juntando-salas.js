21cde849f5470b9f5b991298487b78a00944c386
Bruno Tenório <tenoriobn@gmail.com>
2023-08-04 14:59:01 -0300

    Concateando com concat()

---
+++ juntando-salas.js
@@ -0,0 +1,7 @@
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+//A ordem varias iniciando com a array de fora e depois a de dentro.
+const salasUnificadas = salaJS.concat(salaPython)
+
+console.log(salasUnificadas)
