0c41558bd350fbb0512f889ca5c67cfd8f44a2db
Bruno Tenório <tenoriobn@gmail.com>
2023-08-04 15:17:06 -0300

    Explorando lista com 2 dimensões

---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,19 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias]
+
+console.log(`A aluna da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}.
+A nota dessa aluna é ${listaDeAlunosEMedias[1][1]}.
+`)
+
+/*
+const nomes = ["Ana", "Juliana", "Leonardo"];
+const idades = [30, 35, 28];
+const faculdade = [false, true, true];
+
+const funcionarios = [nomes, idades, faculdade];
+
+console.log(`${funcionarios[0][1]} tem ${funcionarios[1][0]} anos. É verdade que faz faculdade? ${funcionarios[2][2]}`)
+*/
